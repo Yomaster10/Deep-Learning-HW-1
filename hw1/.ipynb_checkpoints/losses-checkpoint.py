321927e648dbdb75d8643cb0bf5c39983c26780b
Yotam Granov <yotam.g@campus.technion.ac.il>
2022-04-27 16:32:37 +0300

    Folders

---
+++ hw1/.ipynb_checkpoints/losses-checkpoint.py
@@ -0,0 +1,100 @@
+import abc
+import torch
+
+
+class ClassifierLoss(abc.ABC):
+    """
+    Represents a loss function of a classifier.
+    """
+
+    def __call__(self, *args, **kwargs):
+        return self.loss(*args, **kwargs)
+
+    @abc.abstractmethod
+    def loss(self, *args, **kw):
+        pass
+
+    @abc.abstractmethod
+    def grad(self):
+        """
+        :return: Gradient of the last calculated loss w.r.t. model
+            parameters, as a Tensor of shape (D, C).
+        """
+        pass
+
+
+class SVMHingeLoss(ClassifierLoss):
+    def __init__(self, delta=1.0):
+        self.delta = delta
+        self.grad_ctx = {}
+
+    def loss(self, x, y, x_scores, y_predicted):
+        """
+        Calculates the Hinge-loss for a batch of samples.
+
+        :param x: Batch of samples in a Tensor of shape (N, D).
+        :param y: Ground-truth labels for these samples: (N,)
+        :param x_scores: The predicted class score for each sample: (N, C).
+        :param y_predicted: The predicted class label for each sample: (N,).
+        :return: The classification loss as a Tensor of shape (1,).
+        """
+
+        assert x_scores.shape[0] == y.shape[0]
+        assert y.dim() == 1
+
+        # TODO: Implement SVM loss calculation based on the hinge-loss formula.
+        #  Notes:
+        #  - Use only basic pytorch tensor operations, no external code.
+        #  - Full credit will be given only for a fully vectorized
+        #    implementation (zero explicit loops).
+        #    Hint: Create a matrix M where M[i,j] is the margin-loss
+        #    for sample i and class j (i.e. s_j - s_{y_i} + delta).
+
+        loss = None
+        # ====== YOUR CODE: ======
+        N = y.shape[0]
+        y_scores = x_scores[[range(N)], y]
+        y_scores = y_scores.squeeze().unsqueeze(1)
+        
+        # Hinge loss matrix
+        M = self.delta + x_scores - y_scores
+        M[M<0] = 0
+        M[[range(N)], y] = 0
+        
+        # In-sample loss, no regularization
+        loss = M.sum() / N
+        # ========================
+
+        # TODO: Save what you need for gradient calculation in self.grad_ctx
+        # ====== YOUR CODE: ======
+        self.grad_ctx = {'M' : M, 'x': x, 'y': y}
+        # ========================
+
+        return loss
+
+    def grad(self):
+        """
+        Calculates the gradient of the Hinge-loss w.r.t. parameters.
+        :return: The gradient, of shape (D, C).
+
+        """
+        # TODO:
+        #  Implement SVM loss gradient calculation
+        #  Same notes as above. Hint: Use the matrix M from above, based on
+        #  it create a matrix G such that X^T * G is the gradient.
+
+        grad = None
+        # ====== YOUR CODE: ======
+        M = self.grad_ctx['M']
+        x = self.grad_ctx['x']
+        y = self.grad_ctx['y']
+        N = x.shape[0]
+        
+        M[M!=0] = 1
+        M[[range(N)],y] = -1 * M.sum(dim=1,dtype=x.dtype)
+        x_T = torch.transpose(x,0,1)
+        
+        grad = (x_T @ M) / N
+        # ========================
+
+        return grad
